fd806b30d8dffae1a1eb7f35bed72012feb1652a
Jtaylorthegreat <46611199+Jtaylorthegreat@users.noreply.github.com>
2019-05-16 15:19:42 -0500

    Update Disk_Test_script.sh
    - Changed log output from /home/$USER to /root due to script trying to write to /home/root when running with elevated privileges (as required by smartmontools) and /home/root not being a valid directory by default.
    
    - Added 60 second countdown message that way log displayed accurate smart test information
    
    - Added date time step to log output

--- Disk_Test_script.sh
+++ Disk_Test_script.sh
@@ -1,18 +1,28 @@
 #!/bin/bash
 # script depends on smartmontools
 # apt-get install smartmontools | yum install smartmontools
+# Must run with elevated privileges
 R="\e[0;49;31m"
 W="\e[0;39m"
 G="\e[1;32m"
+B="\e[1;49;34m"
 smartctl -t short /dev/sda;
 smartctl -t short /dev/sdb;
-sleep 5;
-echo -e $R "---------------------------------------------------------------------------------------------------------------" >>  /home/$USER/"Disk_test$(date '+%F').txt";
-echo -e $G " /dev/sda                                                                                                      " >>  /home/$USER/"Disk_test$(date '+%F').txt";
-echo -e $R "---------------------------------------------------------------------------------------------------------------" $W >>  /home/$USER/"Disk_test$(date '+%F').txt";
-smartctl -l selftest /dev/sda >> /home/$USER/"Disk_test$(date '+%F').txt";
-echo -e $R "---------------------------------------------------------------------------------------------------------------" >>  /home/$USER/"Disk_test$(date '+%F').txt";
-echo -e $G " /dev/sdb                                                                                                      " >>  /home/$USER/"Disk_test$(date '+%F').txt";
-echo -e $R "---------------------------------------------------------------------------------------------------------------" $W >>  /home/$USER/"Disk_test$(date '+%F').txt";
-smartctl -l selftest /dev/sdb >> /home/$USER/"Disk_test$(date '+%F').txt";
-echo -e $R "---------------------------------------------------------------------------------------------------------------" $W >>  /home/$USER/"Disk_test$(date '+%F').txt";
+clear
+for i in {59..01}
+        do
+                echo "Please wait $i more seconds disk test is still running"
+                sleep 1
+                clear
+        done
+
+echo -e $R "---------------------------------------------------------------------------------------------------------------"    >>  /root/"Disk_test$(date '+%F').txt";
+echo -e $G " /dev/sda                                                                                                      "    >>  /root/"Disk_test$(date '+%F').txt";
+echo -e $R "---------------------------------------------------------------------------------------------------------------" $W >>  /root/"Disk_test$(date '+%F').txt";
+smartctl -l selftest /dev/sda >> /root/"Disk_test$(date '+%F').txt";
+echo -e $R "---------------------------------------------------------------------------------------------------------------"    >>  /root/"Disk_test$(date '+%F').txt";
+echo -e $G " /dev/sdb                                                                                                      "    >>  /root/"Disk_test$(date '+%F').txt";
+echo -e $R "---------------------------------------------------------------------------------------------------------------" $W >>  /root/"Disk_test$(date '+%F').txt";
+smartctl -l selftest /dev/sdb >> /root/"Disk_test$(date '+%F').txt";
+echo -e $R "---------------------------------------------------------------------------------------------------------------" $W >>  /root/"Disk_test$(date '+%F').txt";
+echo "Disk Test complete please /root/Disk_test$(date '+%F').txt for more info"
